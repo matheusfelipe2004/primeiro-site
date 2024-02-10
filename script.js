2c93f71a8e2da6ce25188425197597e5acdd84a3
matheusfelipe2004 <matheusfelipe2004.mf@gmail.com>
2024-02-09 23:57:43 -0300

    criei site thebestgame.com

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let matheus = 10
+let matheus2 = 5
+let total = 0
+matheus2 = "Agora é uma strings"
+total = matheus+matheus2
+
+console.log("Eu eu sou o Mathues")
+console.log("boa tarde")
+console.log("O resultado final é " + total)
+
+const numero = 7
+numero = "fabio"
+console.log(numero)
