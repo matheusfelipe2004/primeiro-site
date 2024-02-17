06fbd4d9e2d579c01b82a69279dbd95bb47e9563
matheusfelipe2004 <matheusfelipe2004.mf@gmail.com>
2024-02-16 23:26:17 -0300

    atualizei o hindex.html e o style.css, criando @media screen

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let matheus = 10
 let matheus2 = 5
 let total = 0
-matheus2 = "Agora é uma strings"
+matheus2 = "Agora é uma strings "
 total = matheus+matheus2
 
 console.log("Eu eu sou o Mathues")
@@ -10,5 +10,5 @@ console.log("boa tarde")
 console.log("O resultado final é " + total)
 
 const numero = 7
-numero = "fabio"
+//numero = "fabio"
 console.log(numero)
